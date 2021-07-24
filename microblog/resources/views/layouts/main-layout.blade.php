3a36e437dc02017fe711f6b9a396ab6dca7e539c
Makarin Nikolay <makarin@kgdnet.ru>
2021-07-24 22:18:54 +0200

    Stylization of pagination has changed.

--- microblog/resources/views/layouts/main-layout.blade.php
+++ microblog/resources/views/layouts/main-layout.blade.php
@@ -12,7 +12,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
-    </script><link href="/css/style.css" rel="stylesheet"></script></head>
+    <link href="/css/style.css" rel="stylesheet"></head>
 <body>
 
 <div class="wrapper">
