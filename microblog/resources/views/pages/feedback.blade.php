d908cb352ce0fbcd77d6ad518b7cba28073adc7f
Makarin Nikolay <makarin@kgdnet.ru>
2021-07-26 12:57:28 +0200

    Feedback view added.

---
+++ microblog/resources/views/pages/feedback.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main-layout')
+
+@section('title')
+    Обратная связь
+@endsection
+
+@section('content')
+    <section id="feedback" class="post">
+        <h3 class="title__post"><a href="/feedback" class="link">&#123; // Обратная связь // }</a></h3>
+        <p class="text__post mt-4">
+            Если у вас есть какие-либо вопросы или предложения по поводу данного блога, вы можете оставить сообщение
+            автору данного блога благодаря форме ниже.
+        </p>
+        <p>TODO: Сделать форму тут ...</p>
+    </section>
+@endsection
