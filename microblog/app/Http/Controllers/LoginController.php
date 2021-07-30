de118ab07862450dac165eaef27e8fdb85602b02
Makarin Nikolay <makarin@kgdnet.ru>
2021-07-30 15:47:56 +0200

    LoginController completed.

--- microblog/app/Http/Controllers/LoginController.php
+++ microblog/app/Http/Controllers/LoginController.php
@@ -3,11 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     public function showForm()
     {
         return view('admin.login');
+    }
+
+    public function authentication(Request $request)
+    {
+        $credentials = [
+            'login' => $request->input('login_user'),
+            'password' => $request->input('password_user')
+        ];
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/admin/dashboard');
+        }
+
+        return back()->withErrors([
+            'error' => 'Логин или пароль не верный!',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
     }
 }
