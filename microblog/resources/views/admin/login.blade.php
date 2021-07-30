2dacd1dc5380810ecd47d381715c4a3b282706f5
Makarin Nikolay <makarin@kgdnet.ru>
2021-07-30 15:48:45 +0200

    Login template completed.

--- microblog/resources/views/admin/login.blade.php
+++ microblog/resources/views/admin/login.blade.php
@@ -36,8 +36,7 @@
 
 
     <!-- Custom styles for this template -->
-    <link href="css/signin.css" rel="stylesheet">
-    <script defer src="js/script.bundle.js"></script><link href="css/style.css" rel="stylesheet"></head>
+    <link href="css/signin.css" rel="stylesheet"></head>
 <body>
 
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
@@ -49,16 +48,26 @@
 
 <div class="wrapper">
     <main class="form-signin text-center">
-        <form>
+        <form method="POST" action="/admin/login">
             <h1 style="font-weight: 300;">Микроблог</h1>
             <h2 class="h3 mb-3 fw-normal">Панель администратора</h2>
 
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            @csrf
             <div class="form-floating">
                 <input type="text" class="form-control" id="login_user" name="login_user" placeholder="login">
                 <label for="login_user">Ваше имя пользователя</label>
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" id="password_user" placeholder="Пароль">
+                <input type="password" class="form-control" id="password_user" name="password_user" placeholder="Пароль">
                 <label for="password_user">Ваш пароль</label>
             </div>
 
@@ -80,6 +89,6 @@
 <!-- Option 1: Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="js/dashboard.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script></script>
 </body>
 </html>
