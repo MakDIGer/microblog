806b743d8257000887d456a42c8bdb3d229937f7
Makarin Nikolay <makarin@kgdnet.ru>
2021-08-02 11:08:40 +0200

    Edit-category form and action to update DB added.

--- microblog/app/Http/Controllers/AdminController.php
+++ microblog/app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use \App\Models\Category;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -24,5 +25,29 @@ class AdminController extends Controller
     public function getFeedbacks()
     {
         return view('admin.feedbacks');
+    }
+
+    public function editCategoryShow($id)
+    {
+        $category = Category::where('id', $id)->first();
+        return view('admin.edit-category', [
+            'category' => $category
+        ]);
+    }
+
+    public function editCategory(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'title_edit-category' => 'required|min:4|max:254'
+        ]);
+        $res = DB::table('categories')->where('id',$id)->update(['title' => $validated['title_edit-category']]);
+
+        if ($res) {
+            return $this->getCategories();
+        }
+
+        return back()->withErrors([
+            'error' => 'Ошибка обновления записи в БД!'
+        ]);
     }
 }
