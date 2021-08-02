ee083fbcc4adee8d4ed1a3749a4887d826da75e9
Makarin Nikolay <makarin@kgdnet.ru>
2021-08-02 13:10:18 +0200

    New category show template added.

---
+++ microblog/resources/views/admin/new-category-show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.admin-layout')
+
+@section('content')
+    <h2>Новая категория</h2>
+    <div class="table-responsive">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form method="POST" action="{{ route('new-category') }}">
+            @csrf
+            <div class="mb-3">
+                <label for="title_new-category" class="form-label">Категория</label>
+                <input type="text" class="form-control" id="title_new-category" name="title_new-category" value="">
+            </div>
+            <button type="submit" class="btn btn-primary">Добавить</button>
+        </form>
+    </div>
+@endsection
