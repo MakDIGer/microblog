8501024cd4274e2e847246dbcb2515b2f6f5d9e4
Makarin Nikolay <makarin@kgdnet.ru>
2021-08-12 21:08:33 +0200

    AdminCategoriesController added.

---
+++ microblog/app/Http/Controllers/AdminCategoriesController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class AdminCategoriesController extends Controller
+{
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function getCategories()
+    {
+        $categories = Category::orderByDesc('id')->paginate(10);
+
+        return view('admin.categories', [
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function newCategoryShow()
+    {
+        return view('admin.new-category-show');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\RedirectResponse
+     */
+    public function newCategory(Request $request)
+    {
+        $validated = $request->validate([
+            'title_new-category' => 'required|min:3|max:254'
+        ]);
+
+        $category = new Category();
+        $category->title = $validated['title_new-category'];
+        $category->save();
+
+        if ($category->title === $validated['title_new-category'])
+        {
+            return $this->getCategories();
+        }
+
+        return back()->withErrors([
+            'error' => 'Ошибка добавления новой категории в БД!'
+        ]);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function editCategoryShow($id)
+    {
+        $category = Category::where('id', $id)->first();
+
+        return view('admin.edit-category', [
+            'category' => $category
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\RedirectResponse
+     */
+    public function editCategory(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'title_edit-category' => 'required|min:4|max:254'
+        ]);
+
+        $category = Category::where('id', $id)->first();
+        $category->title = $validated['title_edit-category'];
+        $category->save();
+
+        if ($category->wasChanged())
+        {
+            return $this->getCategories();
+        }
+
+        return back()->withErrors([
+            'error' => 'Ошибка обновления записи в БД!'
+        ]);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\RedirectResponse
+     */
+    public function deleteCategory($id)
+    {
+        $category = Category::where('id', $id);
+        $posts = Post::where('category_id', $id)->get();
+        if (count($posts) > 0)
+        {
+            return back()->withErrors([
+                'error' => 'Ошибка! Категория не пустая!'
+            ]);
+        };
+        $category->delete();
+
+        return $this->getCategories();
+    }
+}
